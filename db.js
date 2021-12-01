97c081613b1ca25747ebfe75acd1edcb61631622
novb1492 <novb1492@naver.com>
2021-12-01 18:10:00 +0900

    node++

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const admin = require('firebase-admin'); 
 // firebase Admin 초기화 
-var serviceAccount = require("C:/Users/Administrator/Desktop/study/studyExpress/tttt-cf9d9-firebase-adminsdk-1ilbf-a9904fabad.json");
+var serviceAccount = require("/Users/kimjunyoung/Desktop/studyExpress/tttt-cf9d9-firebase-adminsdk-1ilbf-a9904fabad.json");
 const firebaseAdmin = admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
 });
