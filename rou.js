293a07273e9e83b8a7ca5e60af02104f904aed38
novb1492 <novb1492@naver.com>
2021-12-03 14:12:37 +0900

    ++

--- rou.js
+++ rou.js
@@ -2,5 +2,5 @@ const router = require("express").Router();
 const controller=require("./controller");
 router.get("/", controller.testParm);
 router.get("/test",controller.testJson);
-router.get("/db/postReceiptSave",controller.testInsert);
+router.post("/db/postReceiptSave",controller.testInsert);
 module.exports = router;
